692102ccbe9fd50f8943b4504555de6e29fd174d
Paul Cunningham <pjcunningham@borsuk.co.uk>
2016-11-12 18:17:26 +0000

    Use form in view

--- app/blueprints/frontend/views.py
+++ app/blueprints/frontend/views.py
@@ -2,46 +2,45 @@
 __author__ = 'Paul Cunningham'
 __copyright = 'Copyright 2016, Paul Cunningham'
 
-# This contains our frontend; since it is a bit messy to use the @app.route
-# decorator style when using application factories, all of our routes are
-# inside blueprints. This is the front-facing blueprint.
-#
-# You can find out more about blueprints at
-# http://flask.pocoo.org/docs/blueprints/
-
-from flask import Blueprint, render_template, flash, redirect, url_for
-from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
+from flask import Blueprint, render_template, flash, redirect, url_for, request, session
 from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
-from markupsafe import escape
 
-# from .forms import SignupForm
 from app.ext import nav
+from app.blueprints.frontend.forms import IpsumV1Form
 
 frontend = Blueprint('frontend', __name__)
 
 nav.register_element('frontend_top', Navbar(View('Lorem Ipsum', '.index'), View('Home', '.index')))
 
 
-@frontend.route('/')
+@frontend.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    # setup a default form with its equivalent REST url
+
+    kwargs = {
+        'resource': session.get('resource') or 'api_v1.paragraphs',
+        'count': session.get('count') or 5,
+        'response_format': session.get('response_format') or 'html'
+    }
+
+    form = IpsumV1Form(**kwargs)
+    resource = kwargs.pop('resource')
+    rest_url = url_for(resource, **kwargs)
+    external_rest_url = url_for(resource, _external=True, **kwargs)
+
+    if form.validate_on_submit():
+
+        session['resource'] = form.resource.data
+        session['count'] = form.count.data
+        session['response_format'] = form.response_format.data
+
+        return redirect(url_for('frontend.index'), code=303)
+
+    # session['resource'] = None
+    # session['count'] = None
+    # session['response_format'] = None
+
+    return render_template('index.html', form=form, iframe_source_url=rest_url, external_rest_url=external_rest_url)
 
 
-# # Shows a long signup form, demonstrating form rendering.
-# @frontend.route('/example-form/', methods=('GET', 'POST'))
-# def example_form():
-#     form = SignupForm()
-#
-#     if form.validate_on_submit():
-#         # We don't have anything fancy in our application, so we are just
-#         # flashing a message when a user completes the form successfully.
-#         #
-#         # Note that the default flashed messages rendering allows HTML, so
-#         # we need to escape things if we input user values:
-#         flash('Hello, {}. You have successfully signed up'
-#               .format(escape(form.name.data)))
-#
-#         # In a real application, you may wish to avoid this tedious redirect.
-#         return redirect(url_for('.index'))
-#
-#     return render_template('signup.html', form=form)
