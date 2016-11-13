716e6143763f8f4c12d9649787bba2bddc270a57
Paul Cunningham <pjcunningham@borsuk.co.uk>
2016-11-13 16:11:06 +0000

    Add html code markup

--- app/blueprints/api_v1/resources.py
+++ app/blueprints/api_v1/resources.py
@@ -21,16 +21,39 @@ HTML_TEMPLATE = '''
 </html>
 '''
 
+HTML_CODE_TEMPLATE = '''
+<!doctype html>
+<html lang=en>
+<head>
+<meta charset=utf-8>
+<title>Lorum Ipsum</title>
+</head>
+<body>
+<code>
+{% for item in items %}
+{{ '<p>' ~ item ~ '</p>'|escape}}<br><br>
+{% endfor %}
+</code>
+</body>
+</html>
+'''
+
 
 TEXT_TEMPLATE = '''
 {%- for item in items %}
-{{item}} {{'\n'}}
+{{item}}
 {% endfor %}
 '''
 
 
 def output_html(data, code, headers=None):
     resp = Response(render_template_string(HTML_TEMPLATE, items=data), mimetype='text/html', headers=headers)
+    resp.status_code = code
+    return resp
+
+
+def output_html_code(data, code, headers=None):
+    resp = Response(render_template_string(HTML_CODE_TEMPLATE, items=data), mimetype='text/html', headers=headers)
     resp.status_code = code
     return resp
 
@@ -48,6 +71,8 @@ class Sentences(Resource):
         items = get_sentences(count)
         if response_format.lower() == 'html':
             return output_html(items, 200)
+        elif response_format.lower() == 'html_code':
+            return output_html_code(items, 200)
         elif response_format.lower() == 'text':
             return output_text(items, 200)
         return {'sentences': items}
@@ -60,6 +85,8 @@ class Paragraphs(Resource):
         items = get_paragraphs(count)
         if response_format.lower() == 'html':
             return output_html(items, 200)
+        elif response_format.lower() == 'html_code':
+            return output_html_code(items, 200)
         elif response_format.lower() == 'text':
             return output_text(items, 200)
         return {'paragraphs': items}
