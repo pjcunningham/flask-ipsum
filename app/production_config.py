c81f0f9ec3a58fb4911d7b0a7ce9922b069d2b6e
Paul Cunningham <pjcunningham@borsuk.co.uk>
2017-11-21 11:26:54 +0000

    Correct  SERVER_NAME

--- app/production_config.py
+++ app/production_config.py
@@ -4,5 +4,5 @@ __copyright = 'Copyright 2016, Paul Cunningham'
 
 SECRET_KEY = '69f1094995486510cdb597cb40403f56f16d079cec3d671d'
 
-SERVER_NAME = 'flask-ipsum'
+SERVER_NAME = 'flask-ipsum.uk'
 PREFERRED_URL_SCHEME = 'https'
